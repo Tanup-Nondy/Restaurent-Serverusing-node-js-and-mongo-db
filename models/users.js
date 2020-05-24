c02c21e24c2c5742aad2f7d7b5b1ecd383c2e9a6
Tanup <tanupcsebu02.tkn@gmail.com>
2020-05-24 21:33:03 +0600

    passport authorization added

--- models/users.js
+++ models/users.js
@@ -1,19 +1,12 @@
 var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
+var passportLocalMongoose=require('passport-local-mongoose');
 var userSchema=new Schema({
-	username:{
-		type:String,
-		required:true,
-		unique:true
-	},
-	password:{
-		type:String,
-		required:true,
-	},
 	admin:{
 		type:Boolean,
 		default:false
 	}
 });
 
+userSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model('User',userSchema);
