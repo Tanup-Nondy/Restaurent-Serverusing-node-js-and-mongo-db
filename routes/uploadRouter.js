00d527b48e8bbdd22054780763a3c62c3e18f36d
Tanup <tanupcsebu02.tkn@gmail.com>
2020-05-26 23:30:11 +0600

    Cross origin resource added

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var authenticate=require('../authenticate');
 var multer=require('multer');
+const cors=require('./cors');
+
 
 
 const storage=multer.diskStorage({
@@ -24,21 +26,22 @@ var jsonParser = bodyParser.json();
 uploadRouter.use(jsonParser);
 
 uploadRouter.route('/')
-.get(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
+.options(cors.corsWithOptions,(req,res)=>{res.sendStatus(200);})
+.get(cors.cors,authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
         res.statusCode=403;
         res.end('Get operation not supported on /imageUpload');
 })
-.post(authenticate.verifyUser,authenticate.verifyAdmin,
+.post(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,
 	upload.single('imageForm'),(req, res) => {
     res.statusCode=200;
     res.setHeader('Content-Type','application/json');
     res.json(req.file);
 })
-.put(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
+.put(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /imageUpload');
 })
-.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
+.delete(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     res.statusCode = 403;
     res.end('Delete operation not supported on /imageUpload');
 });
