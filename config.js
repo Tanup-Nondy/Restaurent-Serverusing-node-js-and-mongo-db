e764758b0305bcfa7241dcb7e8b1d54c1b8372be
Tanup <tanupcsebu02.tkn@gmail.com>
2020-05-27 15:46:07 +0600

    OAuth added with facebook authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
 	'secretKey':'46788-67883-12398-09335',
 	'mongoUrl':'mongodb://localhost:27017/Restaurent',
+	'facebook':{
+		clientId: '2770306243248754',
+		clientSecret:'777a08a0aa41c74711010d07899ce155'
+	}
 }
